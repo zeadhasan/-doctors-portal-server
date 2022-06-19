c370e1483d137538f35802282deb866ee2de565a
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-06-19 16:07:59 +0600

    one booking per user api

--- index.js
+++ index.js
@@ -39,8 +39,12 @@ async function run() {
         app.post('/booking', async (req, res) => {
             const booking = req.body;
             const query = { treatment: booking.treatment, data: booking.data, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
             const result = await bookingCollection.insertOne(booking);
-            res.send(result);
+            return res.send({ success: true, result });
         });
 
 
